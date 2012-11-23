355c111cdda1a07b1857e2e3a9b86121add0825f
Kevin Fitzpatrick <kevin@kfitz.me>
2012-11-23 12:50:04 -0800

    Add YARD docs

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,4 +1,13 @@
 class Greeter
+  # Returns "Hello, world" in either English or Spanish
+  # (defaults to English)
+  #
+  # @param [String] language String declaring the language to use.
+  #   Currently the only options are 'english' & 'spanish'.
+  #   If you pass any other String it will default to English.
+  #
+  # @return [String] "Hello, world!" in the passed language.
+  #
   def self.say_hi(language = 'english')
     translator = Translator.new(language)
     translator.hi
